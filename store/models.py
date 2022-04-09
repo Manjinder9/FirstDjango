10b850f0803733e45c85364cbe2adc9894a3e1e9
Manjinder9 <manjinder.kumar@outlook.com>
2022-04-09 00:38:38 -0400

    adding_giveb_name

--- store/models.py
+++ store/models.py
@@ -30,7 +30,7 @@ class Customers(models.Model):
         (MEMBERSHIP_SILVER , 'Silver'),
         (MEMBERSHIP_GOLD , 'Gold'),
         ]
-    first_name = models.CharField(max_length = 255)
+    given_name = models.CharField(max_length = 255)
     last_name = models.CharField(max_length = 255)
     email = models.EmailField(unique = True)
     phone = models.CharField(max_length = 12)
